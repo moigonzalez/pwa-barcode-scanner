c266ae21d522ae03d03bc53f5915d3af1d33b1f5
Moises Gonzalez <moises.gonzalez@flixbus.com>
2019-01-11 12:32:19 +0100

    Removes transition group

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 import CameraHandler from '../cameraHandler';
 import ProductDisplay from '../productDisplay';
 
 const Main = () => (
   <main>
-    <TransitionGroup>
-      <CSSTransition classNames="fade" timeout={300}>
-        <Switch>
-          <Route exact path='/' component={CameraHandler}/>
-          <Route path='/product/:id' component={ProductDisplay}/>
-        </Switch>
-      </CSSTransition>
-    </TransitionGroup>
+    <Switch>
+      <Route exact path='/' component={CameraHandler}/>
+      <Route path='/product/:id' component={ProductDisplay}/>
+    </Switch>
   </main>
 );
 
