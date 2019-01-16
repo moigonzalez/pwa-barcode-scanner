4726cfc0b857f3c70f8108327e0fb19ca818941e
Moises Gonzalez <moises.gonzalez@flixbus.com>
2019-01-16 15:10:40 +0100

    Adds barcode form

--- src/components/productNotFound/productNotFound.js
+++ src/components/productNotFound/productNotFound.js
@@ -3,9 +3,15 @@ import AddProductInfo from '../addProductInfo';
 
 import styles from './productNotFound.css';
 
-const ProductNotFound = () => (
+const ProductNotFound = (props) => (
   <div className="productNotFound__container">
     <h2 className="productDisplay__title not-found">Product not found 😢</h2>
+    <p>The product with barcode: {props.match.params.id} was not found!</p>
+    <form onSubmit={doSomething}>
+      <p>You can enter manually enter the barcode of the product and try again</p>
+      <input className="textInput" type="text" />
+      <button type="submit">Find</button>
+    </form>
     <AddProductInfo />
   </div>
 );
