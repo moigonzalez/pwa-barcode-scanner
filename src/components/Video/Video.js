7e5100cad974ea3768ea9ca0250e6963598300fb
Moises Gonzalez <moises.gonzalez@flixbus.com>
2019-09-30 20:10:45 +0300

    Removes word

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -96,7 +96,7 @@ class Video extends Component {
             <div className="skeleton__unsopported">
               <div>
                 <p>Your device does not support camera access or something went wrong <span role="img" aria-label="thinking-face">🤔</span></p>
-                <p>You can manually enter the barcode below</p>
+                <p>You can enter the barcode below</p>
                 <BarcodeInputField />
               </div>
             </div>
