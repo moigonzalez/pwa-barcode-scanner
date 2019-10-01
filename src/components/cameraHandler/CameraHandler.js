2fab3cac9f882a83265b43d10d09df85d71dbac3
Moises Gonzalez <moises.gonzalez@flixbus.com>
2019-10-01 16:06:29 +0300

    Refactores camerahandler

--- src/components/cameraHandler/CameraHandler.js
+++ src/components/cameraHandler/CameraHandler.js
@@ -1,4 +1,4 @@
-import React, { lazy, Component, Suspense } from 'react';
+import React, { lazy, Suspense, useState, useEffect } from 'react';
 
 import Camera from 'react-feather/dist/icons/camera';
 import ArrowDown from 'react-feather/dist/icons/arrow-down';
@@ -10,64 +10,57 @@ import './cameraHandler.css';
 
 const Video = lazy(() => import('../Video'));
 
-class CameraHandler extends Component {
+const CameraHandler = () => {
 
-  constructor(...props) {
-    super(...props);
+  const [ isCameraSupported, setCameraSupported ] = useState(false);
+  const [ isCameraEnabled, setCameraEnabled ] = useState(dataHandler.isCameraPermissionGranted());
 
-    this.state = {
-      isCameraSupported: false,
-      isCamEnabled: dataHandler.isCameraPermissionGranted()
-    };
-  }
-
-  componentWillMount() {
-    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      this.setState({
-        isCameraSupported: true
-      });
+  useEffect(() => {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      setCameraSupported(true);
     }
-  }
+  }, [])
 
-  onCamEnabled = () => {
+  const onCamEnabled = () => {
     dataHandler.cameraPermissionGranted();
-    this.setState({
-      isCamEnabled: true
-    });
+    setCameraEnabled(true);
   }
 
-  render() {
-    return (
-      <>
-        {this.state.isCameraSupported ?
-          this.state.isCamEnabled ?
-          <Suspense fallback={<div>Loading...</div>}>
-            <Video />
-          </Suspense>
-          :
+  return (
+    <>
+      {isCameraSupported && isCameraEnabled ?
+        <Suspense fallback={<div>Loading...</div>}>
+          <Video />
+        </Suspense>
+      :
+        ""
+      }
+      {isCameraSupported && !isCameraEnabled ?
+        <>
           <div className="cameraHandler__message">Enable your camera with the button below
-          <br/>
-          <div className="cameraHandler__messageIcon"><ArrowDown size={35}/></div>
+            <br/>
+            <div className="cameraHandler__messageIcon"><ArrowDown size={35}/></div>
           </div>
-          :
-          <div className="cameraHandler__unsopported">
-            <div>
-              <p>Your device does not support camera access or something went wrong <span role="img" aria-label="thinking-face">🤔</span></p>
-              <p>You can enter the barcode below</p>
-              <BarcodeInputField />
-            </div>
-          </div>
-        }
-        {this.state.isCamEnabled ?
-          ''
-          :
-          <button type="button" aria-label="Enable Camera" className="btn__round camera__enable" onClick={this.onCamEnabled}>
+          <button type="button" aria-label="Enable Camera" className="btn__round camera__enable" onClick={onCamEnabled}>
             <Camera />
           </button>
-        }
-      </>
-    );
-  }
+        </>
+        :
+        ""
+      }
+      {!isCameraSupported ?
+        <div className="cameraHandler__unsopported">
+          <div>
+            <p>Your device does not support camera access or something went wrong <span role="img" aria-label="thinking-face">🤔</span></p>
+            <p>You can enter the barcode below</p>
+            <BarcodeInputField />
+          </div>
+        </div>
+        :
+        ""
+      }
+    </>
+  );
 }
 
 export default CameraHandler;
