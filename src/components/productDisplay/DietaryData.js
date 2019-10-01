fe530cfd9d6c1c700609a8bdd50ea4f359e898bb
Moises Gonzalez <moises.gonzalez@flixbus.com>
2019-10-01 16:44:24 +0300

    Refactores dietary data

--- src/components/productDisplay/DietaryData.js
+++ src/components/productDisplay/DietaryData.js
@@ -1,45 +1,39 @@
 import React from 'react';
 
-const DietaryData = (product) => (
-  <div className="productDisplay__section">
+const DietaryData = (product) => {
+  const displayDietaryDataItem = (condition) => {
+    let content;
+
+    if (condition === undefined) {
+      content = 'Unknown ❓';
+    } else {
+      content = condition ? 'Yes' : 'No';
+    }
+
+    return (<span className="productDisplay__list--value">
+      {content}
+    </span>)
+  }
+
+  return (<div className="productDisplay__section">
     <h3 className="productDisplay__sectionTitle">Special Diets</h3>
     <div className="productDisplay__list--item">
-      <span>🌱 Plant Based:</span>
-      <span className="productDisplay__list--value">
-        {product.product.isPlantBased() === undefined ?
-        'Unknown ❓'
-        :
-        product.product.isPlantBased() ? 'Yes' : 'No'}
-      </span>
+      <span><span role="img" aria-label="plant">🌱</span> Plant Based:</span>
+      {displayDietaryDataItem(product.product.isPlantBased())}
     </div>
     <div className="productDisplay__list--item">
-      <span>🌾 Gluten:</span>
-      <span className="productDisplay__list--value">
-        {product.product.containsGluten() === undefined ?
-        'Unknown ❓'
-        :
-        product.product.containsGluten() ? 'Yes' : 'No'}
-      </span>
+      <span><span role="img" aria-label="wheat">🌾</span> Gluten:</span>
+      {displayDietaryDataItem(product.product.containsGluten())}
     </div>
     <div className="productDisplay__list--item">
-      <span>🐄 Lactose:</span>
-      <span className="productDisplay__list--value">
-        {product.product.containsLactose() === undefined ?
-        'Unknown ❓'
-        :
-        product.product.containsLactose() ? 'Yes' : 'No'}
-      </span>
+      <span><span role="img" aria-label="cow">🐄</span> Lactose:</span>
+      {displayDietaryDataItem(product.product.containsLactose())}
     </div>
     <div className="productDisplay__list--item">
-      <span>⚠️ Palm Oil:</span>
-      <span className="productDisplay__list--value">
-        {product.product.containsPalmOil() === undefined ?
-        'Unknown ❓'
-        :
-        product.product.containsPalmOil() ? 'Yes' : 'No'}
-      </span>
+      <span><span role="img" aria-label="warning">⚠️</span> Palm Oil:</span>
+      {displayDietaryDataItem(product.product.containsPalmOil())}
     </div>
-  </div>
-);
+  </div>);
+};
 
 export default DietaryData;
