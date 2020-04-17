f32c2783def444896a0f5c75b2c3c9396591778f
Moises Gonzalez <moises.gonzalez@flixbus.com>
2020-04-17 12:42:00 +0200

    Makes all urls relative

--- cypress/integration/e2e_tests.js
+++ cypress/integration/e2e_tests.js
@@ -1,32 +1,32 @@
 describe('Functional test of the app', () => {
   it('Should show the history page', () => {
-    cy.visit("https://pwascanit.com/");
+    cy.visit("/");
 
     cy.get('[href="/history"]').click();
     cy.url().should('include', '/history');
   });
 
   it('Should turn on the camera or request access', () => {
-    cy.visit("https://pwascanit.com/");
+    cy.visit("/");
 
     cy.get('.btn__round').click();
     cy.get('video').should('be.visible');
   });
 
   it('Should be able to go to a product page', () => {
-    cy.visit("https://pwascanit.com/product/3017620422003");
+    cy.visit("/product/3017620422003");
 
     cy.get('.productDisplay__title').should('be.visible');
   });
 
   it('Should be able to go to /not-found page', () => {
-    cy.visit("https://pwascanit.com/product/not-found?code=barcodetest");
+    cy.visit("/product/not-found?code=barcodetest");
 
     cy.get('.productNotFound__container').should('be.visible')
   });
 
   it('Should be able to go to /not-found page AND enter some product barcode', () => {
-    cy.visit("https://pwascanit.com/product/not-found");
+    cy.visit("/product/not-found");
 
     cy.get('.textInput').type('3017620422003');
     cy.get('.btn').click();
@@ -34,14 +34,18 @@ describe('Functional test of the app', () => {
   });
 
   it('Should have a history with empty state', () => {
-    cy.visit("https://pwascanit.com/history");
+    cy.clearLocalStorage();
+    cy.visit("/history");
 
     cy.get('.history__list').children('.history__listItem').should('have.length', 0);
   });
 
   it('Should include one item in history after searching for one', () => {
-    cy.visit("https://pwascanit.com/product/3017620422003");
-    cy.visit("https://pwascanit.com/history");
+    cy.server();
+    cy.visit("/product/3017620422003");
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(500);
+    cy.visit("/history");
 
     cy.get('.history__list').children('.history__listItem').should('have.length', 1);
   });
